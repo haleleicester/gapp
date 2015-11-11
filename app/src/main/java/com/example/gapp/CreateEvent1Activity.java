2baaca1dce33df3e577d56c3629710fc317912a6
Liam Hackett <liamjhackett@gmail.com>
2015-11-11 12:52:00 +0000

    Added Alert Dialog
    Added Alert Diaglog to the TODO button.

--- app/src/main/java/com/example/gapp/CreateEvent1Activity.java
+++ app/src/main/java/com/example/gapp/CreateEvent1Activity.java
@@ -1,11 +1,15 @@
 package com.example.gapp;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +17,8 @@ import android.widget.Toast;
 
 import com.example.gapp.model.Event;
 import com.example.gapp.model.EventWriter;
+
+import java.util.ArrayList;
 
 public class CreateEvent1Activity extends AppCompatActivity {
 
@@ -38,8 +44,42 @@ public class CreateEvent1Activity extends AppCompatActivity {
         String e_name = t1.getText().toString();
         String e_location = t2.getText().toString();
         String e_date = t3.getText().toString();
-       db.addEvent(e_name, e_location, e_date);
+       // db.addEvent(e_name, e_location, e_date);
         // Toast.makeText(this, db.getSale(0).get_contents(), Toast.LENGTH_SHORT).show();
+    }
+
+    public void onToDoClick(View view){
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        // set title
+        alertDialogBuilder.setTitle("Your Title");
+
+        // set dialog message
+        alertDialogBuilder
+                .setMessage("Click yes to exit!")
+                .setCancelable(false)
+                .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        // if this button is clicked, close
+                        // current activity
+                        Log.v("Works", "THIS WORKS");
+                    }
+                })
+                .setNegativeButton("No",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        // if this button is clicked, just close
+                        // the dialog box and do nothing
+                        dialog.cancel();
+                    }
+                });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+
     }
 
 }
