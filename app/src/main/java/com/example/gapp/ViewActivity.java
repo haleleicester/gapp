607622044fbe6e6612f982f565fd77b739cea043
Liam Hackett <liamjhackett@gmail.com>
2015-11-11 16:25:11 +0000

    Added View, showing item from database

--- app/src/main/java/com/example/gapp/ViewActivity.java
+++ app/src/main/java/com/example/gapp/ViewActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.gapp.model.Event;
 import com.example.gapp.model.EventWriter;
@@ -22,12 +24,18 @@ public class ViewActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        EventWriter db = new EventWriter(this);
-        List<Event> events = db.getEvents();
 
-        for (Event e : events) {
-            Log.d("gapp_debug", e.get_id() + ": " + e.get_name() + " " + e.getLocation() + " " + e.getDate() + " " + e.getTime() + " " + e.getCategory() + " " +  e.getDescription() + " " + e.getTarget() + " " + e.getAmountR() + e.getTodo());
-        }
+        EventWriter db = new EventWriter(this);
+        Event e = db.getEvents().get(0);
+
+        TextView database = (TextView) findViewById(R.id.viewText);
+
+
+        database.setText(e.get_name() + "\n" + e.getLocation() + "\n" + e.getDate() + "\n" + e.getTime() + "\n" + e.getCategory() + "\n" +  e.getDescription() + "\n" + e.getTarget() + "\n" + e.getAmountR() + "\n" + e.getTodo());
+
+//        for (Event e : events) {
+//            Log.d("gapp_debug", e.get_id() + ": " + e.get_name() + " " + e.getLocation() + " " + e.getDate() + " " + e.getTime() + " " + e.getCategory() + " " +  e.getDescription() + " " + e.getTarget() + " " + e.getAmountR() + e.getTodo());
+//        }
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
